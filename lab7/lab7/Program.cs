788b443241778e81209e73c978c93b0e66ae578d
lukehoang <hoangminhmung@gmail.com>
2018-10-23 01:50:34 -0400

    coding

--- lab7/lab7/Program.cs
+++ lab7/lab7/Program.cs
@@ -6,10 +6,80 @@ using System.Threading.Tasks;
 
 namespace lab7
 {
-    class Program
+    public class Program
     {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
         static void Main(string[] args)
         {
+
+        }
+
+        public void createBoard()
+
+        {
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
+
+        }
+
+        public char currentPlayer(char currentPlayer)
+        {
+            return currentPlayer;
+        }
+
+        public char changePlayer(char currentPlayer)
+        {
+            if (currentPlayer == 'X')
+            {
+                currentPlayer = 'O';
+            }
+            else
+            {
+                currentPlayer = 'X';
+            }
+
+            return currentPlayer;
+        }
+
+        public void makeMove(char player, out int choice)
+        {
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (player == 'X')
+            {
+                arr[choice] = 'X';
+            }
+            else
+                arr[choice] = 'O';
+
+        }
+
+        public bool spaceInUse(int position)
+        {
+            if (arr[position] == 'O' || arr[position] == 'X')
+            {
+                return false;
+            }
+            else
+                return true;
         }
     }
 }
---
+++ lab7/lab7Tests/ProgramTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+
+        char player = 'X';
+
+        [TestMethod()]
+        public void currentPlayerTestX()
+        {
+            var ttt = new Program();
+
+            char playerTest = ttt.currentPlayer(player);
+
+            Assert.AreEqual('X', playerTest);
+        }
+
+        [TestMethod()]
+        public void currentPlayerTestO()
+        {
+            var ttt = new Program();
+
+            char playerTest = ttt.currentPlayer(player);
+            playerTest = ttt.changePlayer(playerTest);
+            Assert.AreEqual('O', playerTest);
+        }
+
+        [TestMethod()]
+        public void makeMoveTestX()
+        {
+            var ttt = new Program();
+
+            char playerTest = ttt.currentPlayer(player);
+            int choice;
+
+            Console.WriteLine("now is your turn, let make your move by type in a number according to the board' field");
+            ttt.createBoard();
+            ttt.makeMove(player, choice);
+
+            char spot = ttt.ar
+        }
+
+
+    }
+}
